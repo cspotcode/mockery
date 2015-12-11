b205054573a7585df71e6004bded81cf1c2daa16
Andrew Bradley <abradley@brightcove.com>
2015-12-11 17:09:00 -0500

    Resolves #7: Mockery is immune to its own cache clearing

--- mockery.js
+++ mockery.js
@@ -1,6 +1,12 @@
+var Module = require('module');
+
 // Delete ourselves from `require()` cache so that each `require()` call runs this file again.
 // This means `module.parent` will always be the module that `require()`d us, allowing us to
 // resolve relative module paths correctly.
-delete require('module')._cache[module.id];
+delete Module._cache[module.id];
 
-module.exports = require('./lib/mockery').init(module.parent);
+// Store our implementation onto Module so that we are immune to module cache clearing.
+var mockeryLibProp = '__mockery_lib__';
+var mockeryLib = Module[mockeryLibProp] || (Module[mockeryLibProp] = require('./lib/mockery'));
+
+module.exports = mockeryLib.init(module.parent);
---
+++ test/clean-cache.js
@@ -0,0 +1,31 @@
+/*
+This file named 1-general.js for a reason, it is so this
+test is executed first so we can catch the "warmup" of the
+module and test the dead code cases.
+*/
+var vows = require('vows'),
+    assert = require('assert'),
+    m = require('module'),
+    mockery = require('../mockery');
+
+var tests = {
+    'when mockery is required while useCleanCache is enabled': {
+        topic: function () {
+            mockery.enable({useCleanCache: true});
+            mockery.registerMock('fs', 'not really fs');
+            mockery.registerMock('./fixtures/fake_module', {foo: function() {return 'not really fake_module'}});
+            return require('../mockery');
+        },
+        'mockery\'s internal state is still shared': function (mockery) {
+            mockery.deregisterMock('./fixtures/fake_module');
+            assert.equal(require('./fixtures/fake_module').foo(), 'real foo');
+            assert.equal(require('fs'), 'not really fs');
+        },
+        teardown: function() {
+            mockery.deregisterAll();
+            mockery.disable();
+        }
+    }
+};
+
+vows.describe('clean-cache').addBatch(tests).export(module);
